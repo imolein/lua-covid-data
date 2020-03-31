71cc2bad52da1dfd0a19b57e6e01924b073c848b
Sebastian Huebner <sebastian@hueb-ner.de>
2020-03-31 15:00:07 +0200

    updated tests

--- spec/covid-data_spec.lua
+++ spec/covid-data_spec.lua
@@ -1,6 +1,9 @@
 package.path = './src/?.lua;' .. package.path
 
 local test = require('gambiarra')
+
+-- activate test mode
+_TEST = true
 local cd = require('covid-data')
 
 
@@ -41,65 +44,143 @@ end
 
 io.write('--- Started tests for covid data module\n\n')
 
-test('get_latest()', function()
-  local data, err = cd.get_latest()
+test('privat function build_query()', function()
+  local q1 = cd._build_query({ timelines = 1 })
+  local q2 = cd._build_query({
+    timelines = 0,
+    source = 'jhu',
+    country_code = 'de'
+  })
 
+  local patt = '^[%a_]+=%w+%&[%a_]+=%w+%&[%a_]+=%w+$'
+  local function query_matched(query)
+    return query:find(patt) and true or false
+  end
+
+  ok(eq(q1, 'timelines=1'), 'query 1 correctly build')
+  ok(eq(query_matched(q2), true), 'query 2 correctly build')
+end)
+
+test('privat function with_timelines()', function ()
+  ok(eq(cd._with_timelines(true), 1), 'with timelines')
+  ok(eq(cd._with_timelines(false), 0), 'without timelines')
+  ok(eq(cd._with_timelines('miep'), 0), 'params other than boolean return 0')
+end)
+
+test('privat function parse_body()', function ()
+  local ok1, data1 = cd._parse_body('{ "miep": "no" }')
+  local ok2, data2 = cd._parse_body('{ "miep": "no"')
+
+  ok(eq(ok1, true), 'returns true as first value for valid JSON')
+  ok(eq(type(data1), 'table'), 'returns the parsed JSON data as table as second value')
+  ok(eq(ok2, false), 'returns false as first value for invalid JSON')
+  ok(eq(type(data2), 'string'), 'returns error as second value')
+end)
+
+test('get_latest()', function()
+  local data = cd.get_latest()
+
+  ok(err == nil, 'no error')
   ok(eq(type(data), 'table'), 'table is returned')
-  ok(eq(has(data, 'confirmed'), true), 'correct data in table')
+  ok(eq(has(data, 'latest'), true), 'has latest table')
+end)
+
+test('get_latest() with source', function()
+  local data = cd.get_latest('csbs')
+  ok(err == nil, 'no error')
+  ok(eq(type(data), 'table'), 'table is returned')
+  ok(eq(has(data, 'latest'), true), 'has latest table')
 end)
 
 test('get_locations()', function()
-  local data, err = cd.get_locations()
+  local data = cd.get_locations()
 
   ok(eq(type(data), 'table'), 'table is returned')
-  ok(eq(type(data[1]), 'table'), 'returned table is an array of tables')
-  ok(eq(has(data[1], 'country'), true), 'correct data in tables')
-  ok(eq(has(data[1], 'timelines'), false), 'no timelines')
+  ok(eq(has(data, 'locations'), true), '"locations" subtable in received data')
+  ok(eq(has(data, 'latest'), true), '"latest" subtable in received data')
+  ok(eq(type(data.locations[1]), 'table'), 'returned table is an array of tables')
+  ok(eq(has(data.locations[1], 'country'), true), 'correct data in tables')
+  ok(eq(has(data.locations[1], 'timelines'), false), 'no timelines')
+end)
+
+test('get_locations() with source', function()
+  local data = cd.get_locations(true, 'csbs')
+
+  ok(eq(type(data), 'table'), 'table is returned')
+  ok(eq(data.locations[1].country, 'US'), 'country is US cause "csbs" source has only US data')
+  ok(eq(has(data.locations[1], 'timelines'), false), '"csbs" source has no timelines')
 end)
 
 test('get_locations() with timelines', function()
-  local data, err = cd.get_locations(true)
+  local data = cd.get_locations(true)
 
   ok(eq(type(data), 'table'), 'table is returned')
-  ok(eq(type(data[1]), 'table'), 'returned table is an array of tables')
-  ok(eq(has(data[1], 'timelines'), true), 'has timelines')
+  ok(eq(has(data.locations[1], 'timelines'), true), 'has timelines')
 end)
 
 test('get_locations_by_code()', function()
-  local data, err = cd.get_by_location_code('de')
+  local data = cd.get_by_location_code('de')
 
   ok(eq(type(data), 'table'), 'table is returned')
-  ok(eq(has(data, 'country'), true), 'correct data in tables')
+  ok(eq(has(data, 'locations'), true), '"locations" subtable in received data')
+  ok(eq(has(data, 'latest'), true), '"latest" subtable in received data')
+  ok(eq(data.locations[1].country_code, 'DE'), 'correct data in tables')
+end)
+
+test('get_locations_by_code() with source', function()
+  local data = cd.get_by_location_code('us', true, 'csbs')
+
+  ok(eq(type(data), 'table'), 'table is returned')
+  ok(eq(data.locations[1].country, 'US'), 'country is US cause "csbs" source has only US data')
+  ok(eq(has(data.locations[1], 'timelines'), false), '"csbs" source has no timelines')
 end)
 
 test('get_locations_by_code() with timelines', function()
-  local data, err = cd.get_by_location_code('de', true)
+  local data = cd.get_by_location_code('de', true)
 
   ok(eq(type(data), 'table'), 'table is returned')
-  ok(eq(has(data, 'country'), true), 'correct data in tables')
-  ok(eq(has(data, 'timelines'), true), 'has timelines')
+  ok(eq(has(data.locations[1], 'timelines'), true), 'has timelines')
 end)
 
 test('get_locations_by_id()', function()
-  local data, err = cd.get_by_location_id(11)
+  local data = cd.get_by_location_id(11)
 
   ok(eq(type(data), 'table'), 'table is returned')
-  ok(eq(has(data, 'country'), true), 'correct data in tables')
+  ok(eq(has(data.location, 'country'), true), 'correct data in tables')
+  ok(eq(has(data.location, 'timelines'), true), 'timelines table in data even if "timelines=0"...')
+  ok(#data.location.timelines, '...but it is empty')
+end)
+
+test('get_locations_by_id() with source', function()
+  local data = cd.get_by_location_id(11, true, 'csbs')
+
+  ok(eq(type(data), 'table'), 'table is returned')
+  ok(eq(data.location.country, 'US'), 'country is US cause "csbs" source has only US data')
+  ok(eq(has(data.location, 'timelines'), true), 'timelines table in data even if "csbs" data source has not timeline data')
+  ok(#data.location.timelines, '...but it is empty')
 end)
 
 test('get_locations_by_id() with timelines', function()
-  local data, err = cd.get_by_location_id(11, true)
+  local data = cd.get_by_location_id(11, true)
 
   ok(eq(type(data), 'table'), 'table is returned')
-  ok(eq(has(data, 'country'), true), 'correct data in tables')
-  ok(eq(has(data, 'timelines'), true), 'has timelines')
+  ok(eq(has(data.location, 'timelines'), true), 'has timelines table...')
+  ok(eq(has(data.location.timelines, 'confirmed'), true), '...which is not empty')
 end)
 
 test('get_sources()', function()
-  local data, err = cd.get_sources()
+  local data = cd.get_sources()
 
   ok(eq(type(data), 'table'), 'table is returned')
-  ok(data:len() > 0, 'returned data is an arry > 0')
+  ok(#data.sources > 0, 'returned data is an arry > 0')
+end)
+
+test('errors', function()
+  local data, err, raw = cd.get_by_location_code('de', false, 'csbs')
+
+  ok(eq(data, false), 'country_code "DE" and source "csbs" is not available')
+  ok(eq(err, 404), 'it returns a 404')
+  ok(eq(raw, '{"detail":"Source `csbs` does not have the desired location data."}'), 'and an error message as JSON, which is returned raw')
 end)
 
 report()
